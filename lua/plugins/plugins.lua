8ad618918cf88fedc4da726cb6020784e37f73f0
Yaroslav <zaruchevskiy@stm32f0.ru>
2025-01-09 12:47:48 +0000

    initial

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,64 @@
+local plugins = {
+  {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "pyright",
+      "mypy",
+      "ruff",
+      "debugpy",
+    },
+  },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft={"python"},
+    opts = function()
+      return require "configs.null-ls"
+    end,
+
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+    end,
+
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function ()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "nvim-neotest/nvim-nio",
+  }
+
+}
+
+return plugins
