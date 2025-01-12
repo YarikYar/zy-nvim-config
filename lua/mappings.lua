b65699b44f0f7360fa213a51fe4c0b1fb28c4ed5
Yaroslav <yaroslav@vm736994.cloud.nuxt.network>
2025-01-12 20:59:55 +0100

    add bindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map('n', '<leader>dcb', ':!docker compose up -d --build<cr>', { noremap = true, silent = true })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
